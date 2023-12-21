2394e9c7c2416a039233c0390c615318034eb706
Ronnie <nicandro.ronnie@gmail.com>
2023-12-21 23:27:41 +0800

    updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -47,9 +47,17 @@ export default function Home() {
       {perks.map((perk) => (
         <div key={perk.name} className="text-center md:flex md:items-start md:text-left lg:block lg:text-center">
           <div className="md:flex-shrink-0 flex justify-center">
-          <div className="h-16 w-16 flex items-center justify-center rounded-full ">
-            {<perk.icon />}
+          <div className="h-16 w-16 flex items-center justify-center rounded-full bg-blue-100 text-blue-600">
+            {<perk.icon  className="w-1/3 h-1/3"/>}
           </div>
+          </div>
+          <div className="mt-6 md:ml-4 md:mt-0 lg:ml-0 lg:mt-6">
+            <h3 className="text-base font-medium text-gray-900">
+              {perk.name}
+            </h3>
+          <p className="mt-3 text-sm text-muted-foreground">
+            {perk.description}
+            </p>
           </div>
         </div>
       ))}
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import MaxWidthWrapper from "./MaxWidthWrapper";
+import Link from "next/link";
+import { Icons } from "./Icons";
+const Navbar = () => {
+  return (
+     <div className="bg-white sticky z-50 top-0 inset-x-0 h-16">
+      <header className="relative bg-white">
+        <MaxWidthWrapper>
+          <div className="border-b border-gray-200">
+            <div className="flex h-16 items-center">
+              {/* TODO:MOBILE */}
+
+              <div className="ml-4 flex lg:ml-0">
+                <Link href="/">
+                  <Icons.logo  className="h-10 w-10"/>
+                </Link>
+              </div>
+            </div>
+          </div>
+        </MaxWidthWrapper>
+      </header>
+    </div>
+  );
+};
+
+export default Navbar;
