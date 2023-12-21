3e8d2401e9a2d5f53fcb0760b4a18c5655bad1dd
Ronnie <nicandro.ronnie@gmail.com>
2023-12-21 23:32:49 +0800

    updated

---
+++ src/components/NavItems.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NavItems = () => {
+  return (
+    <div className='flex'>
+
+    </div>
+  )
+}
+
+export default NavItems
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import MaxWidthWrapper from "./MaxWidthWrapper";
 import Link from "next/link";
 import { Icons } from "./Icons";
+import NavItems from "./NavItems";
 const Navbar = () => {
   return (
      <div className="bg-white sticky z-50 top-0 inset-x-0 h-16">
@@ -15,6 +16,9 @@ const Navbar = () => {
                 <Link href="/">
                   <Icons.logo  className="h-10 w-10"/>
                 </Link>
+              </div>
+              <div className="hidden z-50 lg:ml-7 lg:block lg:self-stretch">
+              <NavItems />
               </div>
             </div>
           </div>
