aae7b78684ae8b613767348c9002d18c801c6e29
Ronnie <nicandro.ronnie@gmail.com>
2024-01-01 20:09:48 +0800

    updated

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -1,8 +1,23 @@
-import React from 'react'
 
+import React from 'react'
+import {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetDescription,
+  SheetFooter,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "./ui/sheet"
+import { ShoppingCart } from 'lucide-react'
 const Cart = () => {
   return (
-    <div>Cart</div>
+    <Sheet>
+      <SheetTrigger>
+      <ShoppingCart className='h-6 w-6 flex-shrink-0 text-gray-400 group-hover:text-gray-500'/>
+      </SheetTrigger>
+    </Sheet>
   )
 }
 
