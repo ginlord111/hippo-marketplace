21fba5da5dfbaa4dec59e90f621d0c85379c440f
Ronnie <nicandro.ronnie@gmail.com>
2024-01-03 08:50:18 +0800

    updated

---
+++ src/app/(auth)/sign-up/constant.ts
@@ -0,0 +1,8 @@
+
+import * as z from "zod"
+export const formSchema = z.object({
+    username: z.string().min(2, {
+      message: "Username must be at least 2 characters.",
+    }),
+  })
+
--- src/app/(auth)/sign-up/page.tsx
+++ src/app/(auth)/sign-up/page.tsx
@@ -1,41 +1,69 @@
-'use client'
-import { Icons } from '@/components/Icons'
-import { buttonVariants } from '@/components/ui/button'
-
-import { ArrowRight } from 'lucide-react'
-import Link from 'next/link'
-import React from 'react'
+"use client";
+import { Icons } from "@/components/Icons";
+import { buttonVariants } from "@/components/ui/button";
+import { formSchema } from "./constant";
+import { ArrowRight } from "lucide-react";
+import Link from "next/link";
+import React from "react";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import * as z from "zod";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
 
 const page = () => {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      username: "",
+    },
+  });
+
+  //submit function
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    console.log(values)
+  }
+
   return (
- <>
- <div className='container relative flex pt-20 flex-col items-center justify-center lg:px-5'>
-    <div className='mx-auto flex w-full flex-col justify-center space-y-3 '>
-      <div className='flex flex-col items-center space-y-3 text-center'>
-        <Icons.logo  className='h-20 w-20'/>
-      <div className='text-2xl font-bold'>
-        Create an account
-      </div>
-      <Link href='sign-in' className={buttonVariants({variant:'link', className:'gap-1.5'})}>
-        Already have an account? Sign in
-        <ArrowRight  className='h-4 w-4'/>
-      </Link>
-      </div>
-      
-      <div className='grid gap-6'>
-      <form onSubmit={}>
-        <div className='grid gap-2'>
-          <div className='grid gap-1 py-2'>
-            
+    <>
+      <div className="container relative flex pt-20 flex-col items-center justify-center lg:px-5">
+        <div className="mx-auto flex w-full flex-col justify-center space-y-3 ">
+          <div className="flex flex-col items-center space-y-3 text-center">
+            <Icons.logo className="h-20 w-20" />
+            <div className="text-2xl font-bold">Create an account</div>
+            <Link
+              href="sign-in"
+              className={buttonVariants({
+                variant: "link",
+                className: "gap-1.5",
+              })}
+            >
+              Already have an account? Sign in
+              <ArrowRight className="h-4 w-4" />
+            </Link>
+          </div>
+
+          <div className="grid gap-6">
+            <Form {...form}>
+              <form onSubmit={form.handleSubmit(onSubmit)}>
+                <div className="grid gap-2">
+                  <div className="grid gap-1 py-2"></div>
+                </div>
+              </form>
+            </Form>
           </div>
         </div>
-
-      </form>
       </div>
-    </div>
- </div>
- </>
-  )
-}
+    </>
+  );
+};
 
-export default page
+export default page;
