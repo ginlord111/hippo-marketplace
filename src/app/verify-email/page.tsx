2edc7e6c77ade75357a93f83bc89c34d59a6b55a
Ronnie <nicandro.ronnie@gmail.com>
2024-01-09 16:27:02 +0800

    updated

--- src/app/verify-email/page.tsx
+++ src/app/verify-email/page.tsx
@@ -5,20 +5,30 @@ interface VerifyEmailProps {
     [key: string]: string | undefined | string[];
   };
 }
-// THIS INTERFACE IS FOR DYNAMIC INTERFACE THAT CAN HAVE MANY KEYS THAT ARE UNKNOWN AND TYPECRIPT WONT CATCH AN ERROR
-const VerifyEmail = ({ searchParams }: VerifyEmailProps) => {
-  const token = searchParams.token;
 
+// THIS INTERFACE IS FOR DYNAMIC INTERFACE THAT CAN HAVE MANY KEYS THAT ARE UNKNOWN AND TYPECRIPT WONT CATCH AN ERROR
+const VerifyEmail = ({ searchParams }:VerifyEmailProps) => {
+  const token = searchParams.token;
+  const toEmail = searchParams.tos
+    console.log(typeof token)
   return (
-    <div className="container relative flex pt-20- flex-col items-center justify-center lg:px-0">
-      <div className="mx-auto flex w-full flex-cocl justify-center space-y-6 sm:w-[350px]">
+    <div className="container relative flex pt-20- flex-col items-center justify-center lg:px-0 h-screen">
+      <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]  items-center h-full">
         {token && typeof token === "string" ? (
           <div className="grid grid-gap-6"></div>
         ) : (
-          <div className="flex h-full flex-col items-center justify-center">
-            <div className="relative mb-4 h-60 60  text-muted-foreground">
-              <Image src="/hippo-email-sent.png" alt="Hippo Email Sent" fill />
+          <div className="flex h-full flex-col items-center justify-center w-full ">
+            <div className="relative mb-4 h-60 w-60  text-muted-foreground">
+              <Image 
+              fill
+              src="/hippo-email-sent.png"
+               alt="Hippo Email Sent"  
+               />
             </div>
+            <h3 className="font-semibold text-2xl">Check Your Email</h3>
+            <p className="text-muted-foreground text-center">We&apos;ve sent verification link to {toEmail ? (
+            <span className="text-semibold text-center"> {toEmail}</span>
+            ) : (<span> your email</span>)}.</p>
           </div>
         )}
       </div>
