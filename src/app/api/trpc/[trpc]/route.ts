f36d8542e8b3d93be94774a050eaa271859aba20
ginlord111 <nicandro.ronnie@gmail.com>
2024-02-04 23:04:50 +0800

    fix

--- src/app/api/trpc/[trpc]/route.ts
+++ src/app/api/trpc/[trpc]/route.ts
@@ -1,14 +1,14 @@
-import { appRouter } from "@/trpc/server";
-import { fetchRequestHandler } from "@trpc/server/adapters/fetch";
-import { createContext } from "@/server";
-const handler = (req: Request) => {
-  fetchRequestHandler({
-    endpoint: "/api/trpc",
-    req,
-    router: appRouter,
-    //@ts-expect-error
-    createContext: () => {},
-  });
-};
+// import { appRouter } from "@/trpc/server";
+// import { fetchRequestHandler } from "@trpc/server/adapters/fetch";
+// import { createContext } from "@/server";
+// const handler = (req: Request) => {
+//   fetchRequestHandler({
+//     endpoint: "/api/trpc",
+//     req,
+//     router: appRouter,
+//     //@ts-expect-error
+//     createContext: () => {},
+//   });
+// };
 
 // export {handler as POST, handler as GET, handler as PUT}
--- src/get-payload.ts
+++ src/get-payload.ts
@@ -1,68 +1,64 @@
-import dotenv from 'dotenv'
-import path from 'path'
-import payload, { Payload } from 'payload'
-import type {InitOptions} from 'payload/config'
-import nodemailer from 'nodemailer'
+import dotenv from "dotenv";
+import path from "path";
+import payload, { Payload } from "payload";
+import type { InitOptions } from "payload/config";
+import nodemailer from "nodemailer";
 dotenv.config({
-    path:path.resolve(__dirname, '../.env')
-})
+  path: path.resolve(__dirname, "../.env"),
+});
 
 const transporter = nodemailer.createTransport({
-    host:'smtp.resend.com',
-    secure:true,
-    port:465,
-    auth:{
-        user:'resend',
-        pass:process.env.RESEND_KEY,
-    },
-})
-let cached = (global as any).payload
+  host: "smtp.resend.com",
+  secure: true,
+  port: 465,
+  auth: {
+    user: "resend",
+    pass: process.env.RESEND_KEY,
+  },
+});
+let cached = (global as any).payload;
 
-if(!cached){
-    cached = (global as any).payload ={
-        client:null,
-        promise:null,
-    }
+if (!cached) {
+  cached = (global as any).payload = {
+    client: null,
+    promise: null,
+  };
 }
 
 interface Args {
-    initOption?:Partial<InitOptions>
+  initOption?: Partial<InitOptions>;
 }
 
-export const getPayloadClient = async ({initOption}:Args = {}):Promise<Payload> =>{
+export const getPayloadClient = async ({
+  initOption,
+}: Args = {}): Promise<Payload> => {
+  if (!process.env.PAYLOAD_SECRET) {
+    throw new Error("PAYLOAD SECRET IS MISSING IN ENVIRONMENTAL VARIABLES");
+  }
 
-    if(!process.env.PAYLOAD_SECRET){
-        throw new Error('PAYLOAD SECRET IS MISSING IN ENVIRONMENTAL VARIABLES')
+  if (cached.client) {
+    return cached.client;
+  }
 
-    }
+  if (!cached.promise) {
+    cached.promise = payload.init({
+      email: {
+        transport: transporter,
+        fromAddress: "onboarding@resend.dev",
+        fromName: "Hippo Marketplace",
+      },
+      secret: process.env.PAYLOAD_SECRET,
+      local: initOption?.express ? false : true,
+      ...(initOption || {}),
+    });
+  }
 
-    if(cached.client){
-        return cached.client
-     }
+  try {
+    cached.client = await cached.promise;
+  } catch (e: unknown) {
+    cached.promise = null;
+    throw e;
+  }
 
-     if(!cached.promise){
-        cached.promise = payload.init({
-            email:{
-                transport:transporter,
-                fromAddress:'onboarding@resend.dev',
-                fromName:'Hippo Marketplace',
-            },
-            secret:process.env.PAYLOAD_SECRET,
-            local:initOption?.express ? false : true,
-            ...(initOption || {})
-        })
-     }
-     	
-     try {
-        cached.client = await cached.promise;
-     } catch (e:unknown) {
-        cached.promise = null
-        throw e;
-        
-     }
-
-     
-
-     return cached.client;
-}
-
+  return cached.client;
+};
