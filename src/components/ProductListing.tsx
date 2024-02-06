6767315ecb0f013acc9ce1bda2dcd7a588b52111
ginlord111 <nicandro.ronnie@gmail.com>
2024-02-07 00:15:49 +0800

    kekw

---
+++ src/components/ProductListing.tsx
@@ -0,0 +1,14 @@
+import { Product } from '@/payload-types'
+import React from 'react'
+interface ProductListingProps {
+  item:Product | null,
+}
+const ProductListing = ({item}: ProductListingProps) => {
+  return (
+  <>
+  
+  </>
+  )
+}
+
+export default ProductListing
--- src/components/ProductReel.tsx
+++ src/components/ProductReel.tsx
@@ -5,6 +5,8 @@ import React from "react";
 import { buttonVariants } from "./ui/button";
 import { TQueryValidator } from "../lib/query-validator";
 import { trpc } from "../trpc";
+import { Product } from "@/payload-types";
+import ProductListing from "./ProductListing";
 interface ProductReelTypes {
   title?: string;
   href: string;
@@ -13,7 +15,7 @@ interface ProductReelTypes {
 const ProductReel = (props: ProductReelTypes) => {
   const FALLBACK_LIMIT = 4;
   const { title, href, query } = props;
-  const { data: items } = trpc.getInfinitProduct.useInfiniteQuery(
+  const { data: items, isLoading } = trpc.getInfinitProduct.useInfiniteQuery(
     {
       limit: query.limit ?? FALLBACK_LIMIT,
       query,
@@ -23,6 +25,13 @@ const ProductReel = (props: ProductReelTypes) => {
     }
   );
   const products = items?.pages.flatMap((item) => item.products);
+  let itemsToSell:(Product | null) [] = [];
+  if(products && products.length){
+    itemsToSell  = products;
+  }
+  else if (isLoading){
+    itemsToSell= new Array(query.limit ?? FALLBACK_LIMIT);
+  }
 
   console.log("data", items);
 
@@ -38,7 +47,11 @@ const ProductReel = (props: ProductReelTypes) => {
       </div>
       <div className="relative">
         <div className="mt-6 flex items-center w-full">
-          <div className="w-full grid grid-cols-2 gap-x-4 gap-y-10 sm:gap-x-7 md:grid-cols-4 md:gap-y-10 lg:gap-x-8"></div>
+          <div className="w-full grid grid-cols-2 gap-x-4 gap-y-10 sm:gap-x-7 md:grid-cols-4 md:gap-y-10 lg:gap-x-8">
+            {itemsToSell.map((item) => (
+              <ProductListing item={item}/>
+            ))}
+          </div>
         </div>
       </div>
     </section>
