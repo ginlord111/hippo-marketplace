58a26e18225666c6ad8884583e64289877fe0beb
ginlord111 <nicandro.ronnie@gmail.com>
2024-02-09 17:54:58 +0800

    kekw

--- src/components/ImageSlider.tsx
+++ src/components/ImageSlider.tsx
@@ -1,36 +1,59 @@
-import React from "react";
+"use client";
+import React, { useEffect, useState } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
-import "swiper/css";
+import { Pagination } from "swiper/modules";
 import Image from "next/image";
 import { Media } from "@/payload-types";
+import "swiper/css";
+import "swiper/css/pagination";
+import type SwiperType from "swiper";
 interface ImageSliderProps {
   urls: Media[];
 }
 const ImageSlider = ({ urls }: ImageSliderProps) => {
+  const [slide, setSlide] = useState<SwiperType | null>(null);
+  const [index, setIndex] = useState<number>(0);
+  const [sliderConfig, setSliderConfig] = useState({
+    isBegin: true,
+    isEnd: index === (urls.length ?? 0) - 1,
+  });
+  useEffect(() => {
+    slide?.on("slideChange", ({ activeIndex }) => setIndex(activeIndex));
+    setSliderConfig({
+      isBegin: index === 0,
+      isEnd: urls.length === (urls.length ?? 0) - 1,
+    });
+  }, [slide, urls]);
   return (
     <div className="group relative bg-zinc-100 aspect-video overflow-hidden rounded-xl">
       <div className="absolute z-10 inset-0 opacity-0 group-hover:opacity-100 transition">
         <button></button>
         <button></button>
       </div>
-      <Swiper className="h-full w-full">
-        <SwiperSlide className="-z-10 relative h-full w-full">
-          {urls.map((image) => {
-            if (typeof image.url !== "string") {
-              return;
-            }
-            return (
+      <Swiper
+        onSwiper={(swiper) => setSlide(swiper)}
+        modules={[Pagination]}
+        className="h-full w-full"
+        slidesPerView={1}
+        spaceBetween={50}
+      >
+        {urls.map((image) => {
+          if (typeof image.url !== "string") {
+            return;
+          }
+          return (
+            <SwiperSlide className="-z-10 relative h-full w-full">
               <Image
-              key={image.id}
+                key={image.id}
                 src={image?.url}
                 fill
                 loading="eager"
                 className="object-cover -z-10 object-center"
                 alt="Product Image"
               />
-            );
-          })}
-        </SwiperSlide>
+            </SwiperSlide>
+          );
+        })}
       </Swiper>
     </div>
   );
--- src/components/ProductPlaceholder.tsx
+++ src/components/ProductPlaceholder.tsx
@@ -4,12 +4,12 @@ import { Skeleton } from "./ui/skeleton";
 const ProductPlaceholder = () => {
   return (
     <div className="flex flex-col w-full ">
-      <div className="relative bg-zinc-500 aspect-video overflow-hidden rounded-xl">
+      <div className="relative bg-zinc-400 aspect-video overflow-hidden rounded-xl">
         <Skeleton className="w-full h-full" />
       </div>
-      <Skeleton className="w-2/3 mt-4 h-4 rounded-lg bg-zinc-500" />
-      <Skeleton className="w-16 mt-2 h-4 rounded-lg bg-zinc-500" />
-      <Skeleton className="w-12 mt-2 h-4 rounded-lg bg-zinc-500" />
+      <Skeleton className="w-2/3 mt-4 h-4 rounded-lg bg-zinc-400" />
+      <Skeleton className="w-16 mt-2 h-4 rounded-lg bg-zinc-400" />
+      <Skeleton className="w-12 mt-2 h-4 rounded-lg bg-zinc4500" />
     </div>
   );
 };
